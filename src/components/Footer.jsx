43ef428b798ca727736b8925e3650bf9884e092a
suffers <72523933+suffers@users.noreply.github.com>
2025-10-24 20:18:17 -0400

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-auto mb-10 text-gray-500 text-sm text-center">
-      <p>© {new Date().getFullYear()} poccha.co • Built with React & Tailwind</p>
-    </footer>
-  );
-}
